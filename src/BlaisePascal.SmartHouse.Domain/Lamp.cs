8c26a03b0daea0479ebd6f69ad05649ce3918a11
Brigliazz <filippo.brigliadori@gmail.com>
2025-10-31 10:08:36 +0100

    commenti ai primi metodi lamp

--- src/BlaisePascal.SmartHouse.Domain/Lamp.cs
+++ src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -2,10 +2,10 @@
 {
     public class Lamp
     {
-        private bool isOn { get; set; }
-        private int lightPower { get; set; }
-        private bool isWireless { get; set; }
-        private string[] ligthColorsArray= new string[0];
+        private bool isOn { get; set; } // true = on , false = off
+        private int lightPower { get; set; }// how much light power the lamp has
+        private bool isWireless { get; set; }// true = wireless , false = wired
+        private string[] ligthColorsArray= new string[0];// array of colors the lamp can emit
 
         // costructor for lamp
         public void lamp(bool ison, int ligthpower , bool iswireless)
@@ -14,12 +14,12 @@
             lightPower = ligthpower;
             isWireless=iswireless;
         }
-
+        //metod for the light on
         public void turnOn()
         {
             isOn = true;
         }
-
+        //metod for the light off
         public void turnOff()
         {
             isOn = false;
