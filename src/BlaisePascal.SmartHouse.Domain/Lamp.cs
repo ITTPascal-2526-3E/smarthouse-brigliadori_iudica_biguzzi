172a02af79ffba7cfb2127b41fe3832605c73543
w11 <w11@PC4707>
2025-10-31 10:04:14 +0100

    inizio costruzione di funzioni

--- src/BlaisePascal.SmartHouse.Domain/Lamp.cs
+++ src/BlaisePascal.SmartHouse.Domain/Lamp.cs
@@ -2,11 +2,27 @@
 {
     public class Lamp
     {
-        private bool isOn { get; }
-        private int lightPower {  get; }
-        private bool isWireless { get; }
+        private bool isOn { get; set; }
+        private int lightPower { get; set; }
+        private bool isWireless { get; set; }
         private string[] ligthColorsArray= new string[0];
 
-        
+        // costructor for lamp
+        public void lamp(bool ison, int ligthpower , bool iswireless)
+        {   
+            isOn=ison;
+            lightPower = ligthpower;
+            isWireless=iswireless;
+        }
+
+        public void turnOn()
+        {
+            isOn = true;
+        }
+
+        public void turnOff()
+        {
+            isOn = false;
+        }
     }
 }
