37a0dceb8621e030b2889700a91b25f21114dab7
brigliazz <filippo.brigliadori@gmail.com>
2025-11-07 10:47:59 +0100

    prima aggiunta di funzionalità per twolampdevice

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -1,36 +1,45 @@
 ﻿using BlaisePascal.SmartHouse.Domain;
 internal class Program
 {
-        static void Main(string[] args)
-        {
-            Lamp lamp = new Lamp(true, 50, true, 60, 18, 23);
-            Console.WriteLine("lamp is on: " + lamp.isOn);
-            Console.WriteLine("lamp light intensity: " + lamp.lightIntensityPropriety);
-            Console.WriteLine("lamp is wireless: " + lamp.isWireless);
-            Console.WriteLine("lamp consumption value: " + lamp.consumationValue);
-            Console.WriteLine("lamp turns on at: " + lamp.lightOnSpecificTime + "h");
-            Console.WriteLine("lamp turns off at: " + lamp.lightOffSpecificTime + "h");
-            lamp.turnOff();
-            Console.WriteLine("lamp is on after turnOff method: " + lamp.isOn);
-            lamp.lightIntensityPropriety = 80;
-            Console.WriteLine("lamp light intensity after setting to 80: " + lamp.lightIntensityPropriety);
-            lamp.setColor("red");
-            Console.WriteLine("lamp actual color after setting to : " + lamp.getColor());
-            lamp.ApllyScheduleNow();
-            Console.WriteLine("lamp is on after CheckAndApplySchedule method: " + lamp.isOn);
-            Console.WriteLine("-----------------------------");
-            EcoLamp ecoLamp = new EcoLamp(false, 15, false, 15,2);
-            Console.WriteLine("ecoLamp is on: " + ecoLamp.isOn);
-            Console.WriteLine("ecoLamp light intensity: " + ecoLamp.lightIntensityProperty);
-            Console.WriteLine("ecoLamp is wireless: " + ecoLamp.isWireless);
+    static void Main(string[] args)
+    {
+        Lamp lamp = new Lamp(true, 50, true, 60, 18, 23);
+        Console.WriteLine("lamp is on: " + lamp.isOn);
+        Console.WriteLine("lamp light intensity: " + lamp.lightIntensityPropriety);
+        Console.WriteLine("lamp is wireless: " + lamp.isWireless);
+        Console.WriteLine("lamp consumption value: " + lamp.consumationValue);
+        Console.WriteLine("lamp turns on at: " + lamp.lightOnSpecificTime + "h");
+        Console.WriteLine("lamp turns off at: " + lamp.lightOffSpecificTime + "h");
+        lamp.turnOff();
+        Console.WriteLine("lamp is on after turnOff method: " + lamp.isOn);
+        lamp.lightIntensityPropriety = 80;
+        Console.WriteLine("lamp light intensity after setting to 80: " + lamp.lightIntensityPropriety);
+        lamp.setColor("red");
+        Console.WriteLine("lamp actual color after setting to : " + lamp.getColor());
+        lamp.ApllyScheduleNow();
+        Console.WriteLine("lamp is on after CheckAndApplySchedule method: " + lamp.isOn);
+        Console.WriteLine("-----------------------------");
+        EcoLamp ecoLamp = new EcoLamp(false, 15, false, 15, 2);
+        Console.WriteLine("ecoLamp is on: " + ecoLamp.isOn);
+        Console.WriteLine("ecoLamp light intensity: " + ecoLamp.lightIntensityProperty);
+        Console.WriteLine("ecoLamp is wireless: " + ecoLamp.isWireless);
         Console.WriteLine("ecoLamp consumption value: " + ecoLamp.consumationValue);
-            Console.WriteLine("ecoLamp max time on: " + ecoLamp.maxTimeOn + "h");
-            ecoLamp.turnOn();
-            Console.WriteLine("ecoLamp is on after turnOn method: " + ecoLamp.isOn);
-            ecoLamp.lightIntensityProperty = 18;
-            Console.WriteLine("ecoLamp light intensity after setting to 18: " + ecoLamp.lightIntensityProperty);
-            ecoLamp.setColor("green");
-            Console.WriteLine("ecoLamp actual color after setting to green: " + ecoLamp.getColor());
-            ecoLamp.EcoActivation();
-            }
+        Console.WriteLine("ecoLamp max time on: " + ecoLamp.maxTimeOn + "h");
+        ecoLamp.turnOn();
+        Console.WriteLine("ecoLamp is on after turnOn method: " + ecoLamp.isOn);
+        ecoLamp.lightIntensityProperty = 18;
+        Console.WriteLine("ecoLamp light intensity after setting to 18: " + ecoLamp.lightIntensityProperty);
+        ecoLamp.setColor("green");
+        Console.WriteLine("ecoLamp actual color after setting to green: " + ecoLamp.getColor());
+        ecoLamp.EcoActivation();
+        Console.WriteLine("--------------------------------------------");
+
+        LampDevice lampDevice = new LampDevice();
+        lampDevice.addLamp(lamp);
+        lampDevice.addEcoLamp(ecoLamp);
+        lampDevice.lampDeviceStatus();
+
+
+
     }
+}
--- src/BlaisePascal.SmartHouse.Domain/LampDevice.cs
+++ src/BlaisePascal.SmartHouse.Domain/LampDevice.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,13 +14,48 @@ namespace BlaisePascal.SmartHouse.Domain
 
         public void addLamp( Lamp lamp)
         { 
-           lampDevice.Add(lamp);
+           if (lampDevice.Count<2)
+            lampDevice.Add(lamp);
         }
 
-        public void addEcoLamp(bool ask, EcoLamp ecolamp)
+        public void addEcoLamp( EcoLamp ecolamp)
         {
+            if (lampDevice.Count < 2)
                 lampDevice.Add(ecolamp);
         }
+
+        public void removeLamp( Lamp lamp)
+        {
+            lampDevice.Remove(lamp);
+        }   
+        public void removeEcoLamp( EcoLamp ecolamp)
+        {
+            lampDevice.Remove(ecolamp);
+        }
+
+        
+
+        public string lampDeviceStatus()
+        {
+            string status = "";
+            foreach (var item in lampDevice)
+            {
+                if (item is Lamp)
+                {
+                    Lamp lamp = (Lamp)item;
+                    status += $"Lamp - isOn: {lamp.isOn}, lightIntensity: {lamp.lightIntensityPropriety}, isWireless: {lamp.isWireless}, consumationValue: {lamp.consumationValue}, lightOnSpecificTime: {lamp.lightOnSpecificTime}, lightOffSpecificTime: {lamp.lightOffSpecificTime}\n";
+                }
+                else if (item is EcoLamp)
+                {
+                    EcoLamp ecolamp = (EcoLamp)item;
+                    status += $"EcoLamp - isOn: {ecolamp.isOn}, lightIntensity: {ecolamp.lightIntensityProperty}, isWireless: {ecolamp.isWireless}, consumationValue: {ecolamp.consumationValue}, maxTimeOn: {ecolamp.maxTimeOn}\n";
+                }
+            }
+            return status;
+        }
+
+
+
 
 
     }
