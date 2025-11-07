d32e91ccf94e94a2843793afdbb5245128653a1f
w11 <w11@PC4707>
2025-11-07 10:23:48 +0100

    Inizio classe twolampdevice

---
+++ src/BlaisePascal.SmartHouse.Domain/LampDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public class LampDevice
+    {
+        ArrayList lampDevice = new ArrayList();
+
+        public void addLamp( Lamp lamp)
+        { 
+           lampDevice.Add(lamp);
+        }
+
+        public void addEcoLamp(bool ask, EcoLamp ecolamp)
+        {
+                lampDevice.Add(ecolamp);
+        }
+
+
+    }
+}
