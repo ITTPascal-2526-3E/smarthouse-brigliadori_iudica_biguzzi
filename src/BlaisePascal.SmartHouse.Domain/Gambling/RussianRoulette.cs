434d2aafbd0f06aadf8b51d4f69a336ff81415f0
w11 <w11@PC4621>
2025-11-18 13:37:26 +0100

    gambling room finita

--- src/BlaisePascal.SmartHouse.Domain/Gambling/RussianRoulette.cs
+++ src/BlaisePascal.SmartHouse.Domain/Gambling/RussianRoulette.cs
@@ -19,7 +19,7 @@ namespace BlaisePascal.SmartHouse.Domain.Gambling
 
         private List<int> redNumber = new List<int> { 1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36 };
         
-        private List<int> blackNumber = new List<int> { 2, 4, 6, 8, 10, 11,13, 15, 17, 20, 22, 24, 26, 18, 29, 31, 33, 35 };
+        private List<int> blackNumber = new List<int> { 2, 4, 6, 8, 10, 11 ,13, 15, 17, 20, 22, 24, 26, 18, 29, 31, 33, 35 };
         public RussianRoulette(int _bet)
         {
             bet = _bet;
