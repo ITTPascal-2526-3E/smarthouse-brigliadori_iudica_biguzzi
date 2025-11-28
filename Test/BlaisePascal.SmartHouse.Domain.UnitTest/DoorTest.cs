2e7ecca13c12fb343918565629ac58b09e10f8a7
w11 <w11@PC4707>
2025-11-28 10:19:04 +0100

    Implementata classe tes di door

---
+++ Test/BlaisePascal.SmartHouse.Domain.UnitTest/DoorTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.Security;
+
+namespace BlaisePascal.SmartHouse.Domain.UnitTest
+{
+    public class DoorTest
+    {
+        [Fact]
+        public void Door_openDoor_unlocked_assertTrue()
+        {
+            Door door = new Door(false, true, 012);
+            door.openDoor();
+            Assert.True(door.isOpen);
+        }
+
+        [Fact]
+        public void Door_openDoor_locked_assertThrow()
+        {
+            Door door = new Door(false, false, 012);
+            Assert.Throws<Exception>(door.openDoor);
+        }
+
+        [Fact]
+        public void Door_closeDoor_unlocked_assertFalse()
+        {
+            Door door = new Door(true, true, 012);
+            door.closeDoor();
+            Assert.False(door.isOpen);
+        }
+
+        [Fact]
+        public void Door_closeDoor_locked_assertThrown()
+        {
+            Door door = new Door(true, false, 012);
+            Assert.Throws<Exception>(door.closeDoor);
+        }
+
+        [Fact]
+        public void Door_unlocked_assertTrue()
+        {
+            Door door = new Door(false, false , 012);
+            door.unlockDoor(012);
+            Assert.True(door.isLocked);
+        }
+
+        [Fact]
+        public void Door_unlocked_wrongCode_assertThrow()
+        {
+            Door door = new Door(false, false, 012);
+            Assert.Throws<Exception>(() => door.unlockDoor(123));
+        }
+
+        [Fact]
+        public void Door_unlocked_doorOpen_assertThrow()
+        {
+            Door door = new Door(true, false, 012);
+            Assert.Throws<Exception>(() => door.unlockDoor(012));
+        }
+
+        [Fact]
+        public void Door_unlocked_doorunlocked_assertThrow()
+        {
+            Door door = new Door(false, true, 012);
+            Assert.Throws<Exception>(() => door.unlockDoor(012));
+        }
+
+        [Fact]
+        public void Door_locked_doorClose_assertFalse()
+        {
+            Door door = new Door(false, false, 012);
+            door.lockDoor();
+            Assert.True(door.isLocked);
+        }
+
+        [Fact]
+        public void Door_locked_doorOpen_assertFalse()
+        {
+            Door door = new Door(true, false, 012);
+            Assert.Throws<Exception>(() => door.lockDoor());
+        }
+
+    }
+}
