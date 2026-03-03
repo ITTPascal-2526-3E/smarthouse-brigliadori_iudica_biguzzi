425a2dc0734a710447df1fe44d20d267834de245
Brigliazz <Filippo.brigliadori@gmail.com>
2026-03-03 12:38:55 +0100

    fix

--- BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lightining/Lamps/InMemoryLampReposiotory.cs
+++ BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lightining/Lamps/InMemoryLampReposiotory.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.IlluminoiseDevice.Repositories;
+
 
 
 namespace BlaisePascal.SmartHouse.Infrastructure.Repositories.Devices.Lightining.Lamps
 {
-    internal class InMemoryLampReposiotory : IlampRepository
+    internal class InMemoryLampReposiotory : ILampRepository
     {
     }
+
+   
 }
