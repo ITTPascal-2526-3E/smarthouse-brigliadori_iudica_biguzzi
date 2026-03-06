479003771c0c6451b09d2ae4b54213876b5af033
Brigliazz <Filippo.brigliadori@gmail.com>
2026-03-06 09:48:42 +0100

    csv e memory infrastructure implementation

--- BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lightining/Lamps/CsvLampRepository.cs
+++ BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lightining/Lamps/CsvLampRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlaisePascal.SmartHouse.Domain.Abstraction.ValueObj;
+using BlaisePascal.SmartHouse.Domain.IlluminoiseDevice;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,100 @@ using System.Threading.Tasks;
 
 namespace BlaisePascal.SmartHouse.Infrastructure.Repositories.Devices.Lightining.Lamps
 {
-    internal class CsvLampRepository
+    public class CsvLampRepository
     {
+        private readonly string _filePath = "lamps.csv";
+
+        public CsvLampRepository()
+        {
+            var solutionRoot = LocalPathHelper.GetSolutionRoot();
+            var dataFolder = Path.Combine(solutionRoot, "Data");
+            Directory.CreateDirectory(dataFolder);
+
+            _filePath = Path.Combine(dataFolder, "lamps.csv");
+            if (!File.Exists(_filePath))
+            {
+                Save(new List<Lamp>());
+            }
+        }
+
+        public List<Lamp> GetAll()
+        {
+            return Load();
+        }
+
+        public Lamp GetById(Guid id)
+        {
+            return Load().First(l => l.Id == id);
+        }
+
+        private void Save(List<Lamp> lamps)
+        {
+            var dtos = lamps;
+            var lines = new List<string>
+            {
+                "Id,Name,Color,Brightness,IsOn,ligthOnspecificTime,ligthOffSpecificTime"
+            };
+
+            foreach (var dto in dtos)
+            {
+                lines.Add(string.Join(",",
+                    dto.Id,
+                    dto.getName,
+                    dto.actualColor,
+                    dto.getBrightness(),
+                    dto.isOn,
+                    dto.lightOnSpecificTime,
+                    dto.lightOffSpecificTime));
+            }
+
+            File.WriteAllLines(_filePath, lines);
+        }
+
+        private List<Lamp> Load()
+        {
+            var lines = File.ReadAllLines(_filePath);
+            var lamps = new List<Lamp>();
+            foreach (var line in lines.Skip(1))
+            {
+                var parts = line.Split(',');
+                var dto = new Lamp(
+                    bool.Parse(parts[4]),
+                    int.Parse(parts[3]),
+                    false, // Assuming isWireless is false for all lamps in this example
+                    10, // Assuming a default consumationValue for all lamps in this example
+                    new Hour(int.Parse(parts[5])), // lightOnSpecificTime
+                    new Hour(int.Parse(parts[6]))  // lightOffSpecificTime
+                );
+                lamps.Add(dto);
+            }
+            return lamps;
+        }
+
+        public void Update(Lamp lamp)
+        {
+            var lamps = Load();
+            var index = lamps.FindIndex(l => l.Id == lamp.Id);
+            if (index >= 0)
+            {
+                lamps[index] = lamp;
+                Save(lamps);
+            }
+        }
+
+        public void Add(Lamp lamp)
+        {
+            var lamps = Load();
+            lamps.Add(lamp);
+            Save(lamps);
+        }
+
+
+        public void Remove(Lamp lamp)
+        {
+            var lamps = Load();
+            lamps.RemoveAll(l => l.Id == lamp.Id);
+            Save(lamps);
+        }
     }
 }
--- BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lightining/Lamps/InMemoryLampReposiotory.cs
+++ BlaisePascal.SmartHouse.Infrastructure/Repositories/Devices/Lightining/Lamps/InMemoryLampReposiotory.cs
@@ -1,17 +1,67 @@
-﻿using System;
+﻿using BlaisePascal.SmartHouse.Domain.Abstraction.ValueObj;
+using BlaisePascal.SmartHouse.Domain.IlluminoiseDevice;
+using BlaisePascal.SmartHouse.Domain.IlluminoiseDevice.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BlaisePascal.SmartHouse.Domain.IlluminoiseDevice.Repositories;
 
 
 
 namespace BlaisePascal.SmartHouse.Infrastructure.Repositories.Devices.Lightining.Lamps
 {
-    internal class InMemoryLampReposiotory : ILampRepository
+    public class InMemoryLampRepository : ILampRepository
     {
+        private readonly List<Lamp> _lamps;
+
+        public InMemoryLampRepository()
+        {
+            _lamps = new List<Lamp>();
+            {
+                new Lamp(true, 20, false, 2, new Hour(12), new Hour(14));
+            }
+        }
+
+
+        public void Add(Lamp lamp)
+        {
+            if (lamp == null)
+            {
+                throw new ArgumentNullException(nameof(lamp));
+            }
+            _lamps.Add(lamp);
+        }
+
+        public List<Lamp> GetAll()
+        {
+            return _lamps;
+        }
+
+
+        public Lamp GetById(Guid id)
+        {
+            return _lamps.FirstOrDefault(l => l.Id == id);
+        }
+
+        public void Remove(Guid id)
+        {
+            foreach (var lamp in _lamps)
+            {
+                if (lamp.Id == id)
+                {
+                    _lamps.Remove(lamp);
+                    break;
+                }
+            }
+        }
+
+        public void Update(Lamp lamp)
+        {
+            //
+        }
+
     }
 
-   
+
 }
